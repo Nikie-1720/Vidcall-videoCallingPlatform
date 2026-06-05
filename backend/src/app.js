f3956ad06f797c0990abd9b180f5670798764a44
Nikie-1720 <tiwarinikita868@gmail.com>
2026-06-05 15:34:58 +0530

    Added MongoDB debug logs

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,11 @@ const io = connectToSocket(server);
 
 
 const PORT = Number(process.env.PORT) || 8000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/vidcall";
+const MONGO_URI = process.env.MONGO_URI;
+
+console.log("MONGO_URI exists:", !!MONGO_URI);
+
+
 
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
@@ -25,7 +29,11 @@ app.use("/api/v1/messages", messageRouter);
 const start = async () => {
     try {
         app.set( "Nikita");
-        const connectionDB = await mongoose.connect(MONGO_URI);
+        if (!MONGO_URI) {
+    throw new Error("MONGO_URI environment variable is missing");
+}
+
+const connectionDB = await mongoose.connect(MONGO_URI);
         console.log(`Connected to MongoDB at ${connectionDB.connection.host}:${connectionDB.connection.port}`);
 
         server.on("error", (error) => {
